b8f0059328df91e99d28eed6cfaf365fb32484ad
Ilya Sereda <ilya.sereda2002@mail.ru>
2024-06-30 15:40:04 +0300

    Complete task 5

---
+++ task5.sql
@@ -0,0 +1,25 @@
+create table stg.employee_actions
+(
+    employee_id UInt32,
+    dt          DateTime,
+    action_id   UInt32,
+    action_type LowCardinality(String),
+    tsd_id      UInt32,
+    dt_load     DateTime
+)
+engine = MergeTree
+partition by toYYYYMMDD(dt)
+order by employee_id
+comment 'Таблица действий сотрудников, совершаемых на складе';
+
+create table direct_log.employee_actions_buf
+(
+    employee_id UInt32,
+    dt          DateTime,
+    action_id   UInt32,
+    action_type LowCardinality(String),
+    tsd_id      UInt32,
+    dt_load     MATERIALIZED now()
+)
+engine = Buffer(stg, employee_actions, 16, 10, 100, 10000, 1000000, 10000000, 100000000)
+comment 'Буферная таблица для заполнения таблицы employee_actions (Действия сотрудников)';
