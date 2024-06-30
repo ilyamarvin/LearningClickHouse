35e8e425c7e4dd5be9bab8f13188bc5e70d06dfe
Ilya Sereda <ilya.sereda2002@mail.ru>
2024-06-30 15:51:38 +0300

    Edit task 6

--- task6.sql
+++ task6.sql
@@ -13,7 +13,15 @@ order by employee_id
 ttl toStartOfDay(dt) + interval 1 day
 comment 'Таблица последних действий сотрудников, совершенных на складе';
 
-create materialized view stg.mv_employee_actions to current.employee_actions as
+create materialized view stg.mv_employee_actions to current.employee_actions
+    (
+        employee_id UInt32,
+        dt          DateTime,
+        action_id   UInt32,
+        action_type LowCardinality(String),
+        tsd_id      UInt32,
+        dt_created  DateTime
+    ) as
     select employee_id
         , dt
         , action_id
